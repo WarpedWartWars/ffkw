4f5120474bcfe2d941d937cc4d7ce75a5421936b
Lúkos Ddraigcrafangau'r <warpedwartwars@gmail.gom>
2025-04-14 15:23:48 -0700

    add basic stuff

---
+++ index.js
@@ -0,0 +1,10 @@
+var winbox = WinBox({
+    title: "z",
+    url: "file:///C:/Users/Admin/Documents/GitHub/ffkw/index.html",
+    onload: function(){console.log("loaded");},
+    overflow: true,
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%"
+});
